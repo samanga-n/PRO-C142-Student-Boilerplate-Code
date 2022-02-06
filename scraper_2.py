be563266615194e21036a58f63b1df42c58aa927
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-07 00:40:18 +0530

    Add files via upload

---
+++ scraper_2.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import time
+import csv
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+browser = webdriver.Edge("./msedgedriver.exe")
+browser.get(START_URL)
+time.sleep(10)
+headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date", "hyperlink"]
+planet_data = []
+
+def scrape():
+    for i in range(1, 490):
+        while True:
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
+            if current_page_num < i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+            elif current_page_num > i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
+            else:
+                break
+        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+            li_tags = ul_tag.find_all("li")
+            temp_list = []
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            hyperlink_li_tag = li_tags[0]
+            temp_list.append("https://exoplanets.nasa.gov"+hyperlink_li_tag.find_all("a", href=True)[0]["href"])
+            planet_data.append(temp_list)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+        print(f"{i} page done 1")
+#Create a new function here
+
+scrape()
+
+#Call the function for each exoplanet
+
+
+# Create a list called final_planet_data and store elementwise data
+
+
+#Create final_csv and write all the scraped data into it
