ebfb13b6d931676d12579167502fc646640f8bae
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2023-11-11 14:44:11 +0530

    Update new_scraper.py

--- new_scraper.py
+++ new_scraper.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import requests
 import time
 import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
 # NASA Exoplanet URL
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
 # Webdriver
-browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 browser.get(START_URL)
 
 time.sleep(10)
@@ -17,14 +19,27 @@ time.sleep(10)
 new_planets_data = []
 
 def scrape_more_data(hyperlink):
-    print(hyperlink)
-    
-    ## ADD CODE HERE ##
+    try:
+        page = requests.get(hyperlink)
+      
+        soup = BeautifulSoup(page.content, "html.parser")
 
+        temp_list = []
 
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+          
+            for td_tag in td_tags:
+                try: 
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+                    
+        new_planets_data.append(temp_list)
 
-
-
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
 
 
 planet_df_1 = pd.read_csv("updated_scraped_data.csv")
