d6c8cf33d95b20033577230a3856a48d12256d14
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2023-11-15 19:56:10 +0530

    Update new_scraper.py

--- new_scraper.py
+++ new_scraper.py
@@ -17,15 +17,27 @@ time.sleep(10)
 new_planets_data = []
 
 def scrape_more_data(hyperlink):
-    print(hyperlink)
-    
-    ## ADD CODE HERE ##
+    try:
+        page = requests.get(hyperlink)
+      
+        soup = BeautifulSoup(page.content, "html.parser")
 
+        temp_list = []
 
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+          
+            for td_tag in td_tags:
+                try: 
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+                    
+        new_planets_data.append(temp_list)
 
-
-
-
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
 
 planet_df_1 = pd.read_csv("updated_scraped_data.csv")
 
